e522906b582336e63927ce00350e2d0c827d77cd
amir-pasalic <amirpasalic144@gmail.com>
2021-04-02 20:40:22 +0200

    Assignment done !

---
+++ ex02/src/App.js
@@ -0,0 +1,13 @@
+//import logo from './logo.svg';
+import './App.css';
+import GetRequstAsyncAwait from './GetRequestAsyncAwait';
+
+function App() {
+  return (
+    <div className="App">
+      <GetRequstAsyncAwait />
+    </div>
+  );
+}
+
+export default App;
